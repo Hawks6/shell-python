6a35cca1b76124bdc3ec28ac3aab90083a604e2a
Hawks6 <praveenkuamr6828@gmail.com>
2026-05-12 23:49:29 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,11 +2,16 @@ import sys
 
 
 def main():
+    
     while True:
         sys.stdout.write("$ ")
     # pass
         command = input()
-        print(f"{command}: command not found")
+        if(command == 'exit'):
+            break
+        else:
+            print(f"{command}: command not found")
+        
 
 if __name__ == "__main__":
     main()
