c76bbb32b0173586707a6e3c9e2c7eb380057587
Hawks6 <praveenkuamr6828@gmail.com>
2026-05-23 22:38:44 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,14 +1,23 @@
+import subprocess
 import sys
 import os
+import subprocess
 
 
 def main():
     
     while True:
         sys.stdout.write("$ ")
+
     # pass
         commands = ['echo','type','exit']
         command = input()
+        found = False
+        command_parts = command.split(' ')
+        command_name = command_parts[0]
+        
+        path = os.environ.get("PATH","")
+        directories = path.split(os.pathsep)
         if(command == 'exit'):
             break
         elif(command.startswith("echo")):
@@ -17,11 +26,8 @@ def main():
             if(command[5:] in commands):
                 print(f"{command[5:]} is a shell builtin")
             else:
-                found = False
-                command_exe = command[5:].strip()
-                path = os.environ.get("PATH","")
-                directories = path.split(os.pathsep)
                 for directory in directories:
+                    command_exe = command[5:].strip()
                     full_path = os.path.join(directory, command_exe)
                     if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
                         print(f"{command[5:]} is {full_path}")
@@ -29,12 +35,21 @@ def main():
                         break
                 if not found:
                     print(f"{command[5:]}: not found")
+        else:
+            for directory in directories:
+                full_path = os.path.join(directory, command_name)
+                if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
+                    subprocess.run([command_name]+ command_parts[1:], executable=full_path)
+                    found =True
+
+            
+
                     
 
 
 
-        else:
-            print(f"{command}: command not found")
+            if not found:
+                print(f"{command}: command not found")
         
 
 if __name__ == "__main__":
