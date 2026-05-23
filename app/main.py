c194e3af3d54603863310b056c6ac8e807dc6128
Hawks6 <praveenkuamr6828@gmail.com>
2026-05-23 21:31:56 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import sys
+import os
 
 
 def main():
@@ -16,10 +17,26 @@ def main():
             if(command[5:] in commands):
                 print(f"{command[5:]} is a shell builtin")
             else:
-                print(f"{command[5:]}: not found")
+                found = False
+                command_exe = command[5:].strip()
+                path = os.environ.get("PATH","")
+                directories = path.split(os.pathsep)
+                for directory in directories:
+                    full_path = os.path.join(directory, command_exe)
+                    if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
+                        print(f"{command[5:]} is {full_path}")
+                        found = True
+                        break
+                if not found:
+                    print(f"{command[5:]}: not found")
+                    
+
+
+
         else:
             print(f"{command}: command not found")
         
 
 if __name__ == "__main__":
+
     main()
